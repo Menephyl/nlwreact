3ac3c7ebfbe8ed29532bc60435928bcc28d88819
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-04 17:13:16 -0300

    Update main.tsx

--- pass-in-web-react/src/main.tsx
+++ pass-in-web-react/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import {App} from './App.tsx'
 import './index.css'
 // componentes e propriedades
+// componentes sao elementos html com estilos compartilhados
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
