875f6f89f46b6fb1ebe140ec0d7f1a263870a68b
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-04 17:31:38 -0300

    Update App.tsx

--- pass-in-web-react/src/App.tsx
+++ pass-in-web-react/src/App.tsx
@@ -1,11 +1,14 @@
-
+function meuBotao(props){
+  return( <button className="button">{props.texto}</button>
+)
+}
 
 export function App() {
 return(
   <div>
    <meuBotao texto="Clique Aqui"/>
   </div>
-)
+  )
 }
   // return <h1>Hello World</h1>
 
