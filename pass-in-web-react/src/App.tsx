7246d04ca1ec7a4b046aa2f360d5c08b54302da1
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-04 16:59:13 -0300

    Update App.tsx

--- pass-in-web-react/src/App.tsx
+++ pass-in-web-react/src/App.tsx
@@ -3,6 +3,14 @@ export function App() {
 return(
   <div>
     <button>Teste</button>
+    <button>Teste</button>
+    <button>Teste</button>
+    <button>Teste</button>
+    <button>Teste</button>
+    <button>Teste</button>
+    <button>Teste</button>
+    <button>Teste</button>
+    <p></p>
   </div>
 )
   // return <h1>Hello World</h1>
